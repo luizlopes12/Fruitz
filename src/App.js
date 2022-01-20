8b7508a063289081369b446697f1b17e19107df8
Luiz Lopes <luizlopes12@outlook.com>
2022-01-20 20:45:18 -0300

    importando api de testes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import teste from './services/api/teste.json'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
+  const {productItems} = teste;
+  console.log(productItems)
   return (
     <div className="App">
-
+      
     </div>
   );
 }
