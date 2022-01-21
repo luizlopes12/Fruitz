367ada1ee606a776c8f37da34bdb2d39ac50dce3
Luiz Lopes <luizlopes12@outlook.com>
2022-01-21 16:26:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import AppRoutes from "./Routes/AppRoutes";
 import Footer from "./components/Footer";
 function App() {
   const [data, setData] = useState([]);
-  useEffect(() => {
+  /*
+  //////////////////
+  Não consegui entrar na api por erro de CORS, então peguei os dados pelo postman e coloquei no localData.json
+  /////////////////
+    useEffect(() => {
     Api.get("/api/fruit/all")
       .then((response) => {
         setData(response);
@@ -17,7 +21,9 @@ function App() {
       });
   }, []);
   data.length === 0 && setData(localData);
-  const productItems = data;
+  */
+
+  const productItems = localData;
   const [cartItems, setCartItems] = useState([]);
   const handleAddProduct = (product) => {
     const productExists = cartItems.find((item) => item.id === product.id);
