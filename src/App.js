723645d52016c1e8c4e440911ac574be060a3ca9
Luiz Lopes <luizlopes12@outlook.com>
2022-01-21 15:58:43 -0300

    formatando os documentos

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
-import React,{useState, useEffect} from 'react';
-import Api from './services/api/api';
-import localData from './services/api/localData.json'
-import {BrowserRouter as Router} from 'react-router-dom';
-import Header from './components/Header';
-import AppRoutes from './Routes/AppRoutes';
-import Footer from './components/Footer'
+import React, { useState, useEffect } from "react";
+import Api from "./services/api/api";
+import localData from "./services/api/localData.json";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import AppRoutes from "./Routes/AppRoutes";
+import Footer from "./components/Footer";
 function App() {
   const [data, setData] = useState([]);
-  useEffect(()=>{
-      Api.get('/api/fruit/all')
-        .then((response)=>{
-          setData(response)
-    }).catch((error)=>{
-        console.log(error)
+  useEffect(() => {
+    Api.get("/api/fruit/all")
+      .then((response) => {
+        setData(response);
       })
-  }, [])
-  data.length === 0 && setData(localData)
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  data.length === 0 && setData(localData);
   const productItems = data;
-  const [cartItems, setCartItems] = useState([])
-  const handleAddProduct = (product) =>{
-    const productExists = cartItems.find((item) => item.id === product.id)
-    productExists ?
-      setCartItems(cartItems.map((item, key) => (item.id === product.id ?
-      {...productExists, quantity: productExists.quantity + 1}: item)))
-    :
-      setCartItems([...cartItems, {...product, quantity: 1}])
-  }
-  const handleRemoveProduct = (product) =>{
-    const productExists = cartItems.find((item) => item.id === product.id)
-    productExists.quantity === 1 ?
-    setCartItems(cartItems.filter((item) => item.id !== product.id))
-    :
-    setCartItems(cartItems.map((item) => item.id === product.id ? {...productExists, quantity: productExists.quantity - 1} : item))
-  }
-  const handleRemoveTotalProducts = (product) =>{
-    const productExists = cartItems.find((item) => item.id === product.id)
+  const [cartItems, setCartItems] = useState([]);
+  const handleAddProduct = (product) => {
+    const productExists = cartItems.find((item) => item.id === product.id);
+    productExists
+      ? setCartItems(
+          cartItems.map((item, key) =>
+            item.id === product.id
+              ? { ...productExists, quantity: productExists.quantity + 1 }
+              : item
+          )
+        )
+      : setCartItems([...cartItems, { ...product, quantity: 1 }]);
+  };
+  const handleRemoveProduct = (product) => {
+    const productExists = cartItems.find((item) => item.id === product.id);
+    productExists.quantity === 1
+      ? setCartItems(cartItems.filter((item) => item.id !== product.id))
+      : setCartItems(
+          cartItems.map((item) =>
+            item.id === product.id
+              ? { ...productExists, quantity: productExists.quantity - 1 }
+              : item
+          )
+        );
+  };
+  const handleRemoveTotalProducts = (product) => {
+    const productExists = cartItems.find((item) => item.id === product.id);
     productExists.quantity >= 0 &&
-    setCartItems(cartItems.filter((item) => item.id !== product.id))
-  }
-  const handleCartClear = () =>{
-    setCartItems([])
-  }
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+  };
+  const handleCartClear = () => {
+    setCartItems([]);
+  };
 
   return (
     <div className="App">
       <Router>
-      <Header cartItems={cartItems}/>
-      <AppRoutes 
-        productItems={productItems} 
-        cartItems={cartItems} 
-        handleAddProduct={handleAddProduct}
-        handleRemoveProduct={handleRemoveProduct}
-        handleRemoveTotalProducts={handleRemoveTotalProducts}
-        handleCartClear={handleCartClear}
+        <Header cartItems={cartItems} />
+        <AppRoutes
+          productItems={productItems}
+          cartItems={cartItems}
+          handleAddProduct={handleAddProduct}
+          handleRemoveProduct={handleRemoveProduct}
+          handleRemoveTotalProducts={handleRemoveTotalProducts}
+          handleCartClear={handleCartClear}
         />
-      <Footer/>
+        <Footer />
       </Router>
     </div>
   );
