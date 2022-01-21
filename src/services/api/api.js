c6711f8ce1ae3ddbecb210195f85f6124d9635c7
Luiz Lopes <luizlopes12@outlook.com>
2022-01-21 16:26:55 -0300

    Update api.js

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://www.fruityvice.com',
+    baseURL: 'https://www.fruityvice.com',
 })
 export default Api;
